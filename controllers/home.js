18d1e459ce9431e1c341312f7a5da75413ba67b5
chuthathip444 <chuthathip444@gmail.com>
2025-03-13 23:37:52 +0700

    IP

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,12 @@
 var express = require('express');
 const router = express.Router();
 var cors = require('cors');
-const moment = require('moment-timezone');
 const pool = require('../config/db');
 require('dotenv').config();
 
-router.get('/', (req, res) => {
-  res.send('Web Application for Research');
-});
+// router.get('/', (req, res) => {
+//   res.send('Web Application for Research');
+// });
 
 // API: นับจำนวนผู้เข้าชม
 router.get('/track', async (req, res) => {
